fb3a9eb6ed6b5b5a88c165261a4eab2788990c47
wantedwriter <github@wezlab.net>
2024-03-28 20:59:10 -0500

    Create diskdrill.sh

---
+++ fragments/labels/diskdrill.sh
@@ -0,0 +1,7 @@
+diskdrill)
+    name="diskdrill"
+    type="dmg"
+    downloadURL="https://dl.cleverfiles.com/diskdrill.dmg"
+    expectedTeamID="A3W62KZY8Z"
+    blockingProcesses=( Disk Drill )
+    ;;
