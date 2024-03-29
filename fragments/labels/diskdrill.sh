11bc2fe323ec0e22b776f712232785e37b308ffb
wantedwriter <github@wezlab.net>
2024-03-28 21:04:54 -0500

    Update diskdrill.sh

--- fragments/labels/diskdrill.sh
+++ fragments/labels/diskdrill.sh
@@ -1,6 +1,7 @@
 diskdrill)
-    name="diskdrill"
+    name="Disk Drill"
     type="dmg"
+    appname="Disk Drill.app"
     downloadURL="https://dl.cleverfiles.com/diskdrill.dmg"
     expectedTeamID="A3W62KZY8Z"
     blockingProcesses=( Disk Drill )
